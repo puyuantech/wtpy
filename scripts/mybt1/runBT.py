7acff7d603906858904fa77d30ad2efa63fbb6d2
hujiaye <ronniehu9812@163.com>
2022-07-12 08:17:08 +0800

    [feat] install_so.py finished & trans csc to dsb & run bt using dsb

--- scripts/mybt1/runBT.py
+++ scripts/mybt1/runBT.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     engine.commitBTConfig()
 
     # 创建策略，注意：日线的时期需要写成d1，code需要符合wt的格式
-    stra_info = MyStraFut(name='mybacktest', code="SZSE.STK.000023", barCnt=50, period="d1", days=30, k1=0.1, k2=0.1)
+    stra_info = MyStraFut(name='mybacktest', code="SZSE.STK.000038", barCnt=50, period="m1", days=30, k1=0.1, k2=0.1)
     # 挂载策略
     engine.set_cta_strategy(stra_info)
 
